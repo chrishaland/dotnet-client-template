14f1d84f500492a9b096174d36791eb53150a153
Chris Håland <chris.haland@knowit.no>
2021-02-19 15:16:47 +0100

    Add test for swagger docs definition

--- backend/Tests.IntegrationTests/Swagger_documentation_tests.cs
+++ backend/Tests.IntegrationTests/Swagger_documentation_tests.cs
@@ -20,5 +20,17 @@ namespace Tests.IntegrationTests
                 StringAssert.Contains("<title>Swagger UI</title>", contentString);
             });
         }
+
+        [Test]
+        public async Task Swagger_docs_definition_availability()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/swagger/v1/swagger.json");
+            var (response, contentString) = await SUT.SendHttpRequest(request);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            });
+        }
     }
 }
